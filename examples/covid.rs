138a04f9a0687203e4ba66342c3dfd14b5ca041c
kindywu <kindywu@outlook.com>
2024-06-02 17:37:16 +0800

    commit

--- examples/covid.rs
+++ examples/covid.rs
@@ -1,50 +1,20 @@
 use anyhow::Result;
-use polars::io::{csv::read::CsvReader, SerReader};
-use polars::prelude::*;
-use std::io::Cursor;
-use tokio::{
-    fs::File,
-    io::{AsyncReadExt, AsyncWriteExt},
-};
-use tracing::info;
+use queryer::query;
 
 #[tokio::main]
 async fn main() -> Result<()> {
     tracing_subscriber::fmt::init();
 
-    let file_name = "owid-covid-latest.csv";
+    let url = "https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/latest/owid-covid-latest.csv";
 
-    let mut data: String = String::new();
-    if let Ok(mut file) = File::open(file_name).await {
-        let n = file.read_to_string(&mut data).await?;
-        info!("read file size {}", n)
-    } else {
-        let url = format!(
-            "https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/latest/{}",
-            file_name
-        );
-        info!("fetch url {}", url);
-        data = reqwest::get(url).await?.text().await?;
-
-        let mut file = File::create(file_name).await?;
-        file.write_all(data.as_bytes()).await?;
-    }
-
-    // 使用 polars 直接请求
-    let df = CsvReader::new(Cursor::new(data)).finish()?;
-    // println!("{:#?}", df);
-    let mask = df.column("new_deaths")?.gt(100)?;
-    let filtered = df.filter(&mask)?;
-    println!(
-        "{:?}",
-        filtered.select([
-            "location",
-            "total_cases",
-            "new_cases",
-            "total_deaths",
-            "new_deaths"
-        ])
+    // 使用 sql 从 URL 里获取数据
+    let sql = format!(
+        "SELECT location name, total_cases, new_cases, total_deaths, new_deaths \
+        FROM {} where new_deaths >= 200 ORDER BY new_cases DESC",
+        url
     );
+    let df1 = query(sql).await?;
+    println!("{:?}", df1);
 
     Ok(())
 }
